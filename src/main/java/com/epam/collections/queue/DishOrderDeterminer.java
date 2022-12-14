8b2e982757a7e6da8384f3d856592d15f5003b0b
DK <dasha.krutsko@gmail.com>
2022-12-15 00:08:56 +0300

    update

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -5,8 +5,24 @@ import java.util.*;
 public class DishOrderDeterminer {
 
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
-       List<Integer> listOfDishes = new ArrayList<>();
-
+        List<Integer> listOfDishes = new ArrayList<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        int i = 1;
+        while (queue.size() != 0) {
+            Iterator<Integer> it = queue.iterator();
+            while (it.hasNext()) {
+                boolean selected = (i % everyDishNumberToEat == 0);
+                Integer elem = it.next();
+                if (selected) {
+                    listOfDishes.add(elem);
+                    it.remove();
+                }
+                i++;
+            }
+        }
         return listOfDishes;
     }
 }
